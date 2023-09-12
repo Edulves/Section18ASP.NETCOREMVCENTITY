f99762f192bd85e244783323cae77a9b5a2b306f
Eduardo Alves <eduardo.lves@outlook.com>
2023-09-12 20:48:27 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,13 +13,15 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
 
         public IActionResult Contact() {
             ViewData["Message"] = "Your contact page.";
+            ViewData["Email"] = "Eduardo.lves@outlook.com";
 
             return View();
         }
